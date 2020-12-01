5b6b87bbc056acca98bff359fec57f567ab07efa
andreico123 <andreicovaci123@gmail.com>
2020-12-01 22:11:05 +0200

    Add Day 2

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/Users/test/Documents/Programming/Rust/advent_of_code_2020/day1/target/debug/day1: /Users/test/Documents/Programming/Rust/advent_of_code_2020/day1/src/main.rs
--- day1/target/debug/deps/day1.d
+++
@@ -1,5 +0,0 @@
-/Users/test/Documents/Programming/Rust/advent_of_code_2020/day1/target/debug/deps/day1: src/main.rs
-
-/Users/test/Documents/Programming/Rust/advent_of_code_2020/day1/target/debug/deps/day1.d: src/main.rs
-
-src/main.rs:
